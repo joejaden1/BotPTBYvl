797b4d9cc98b9beb67f3dde35d08fd598c2bce30
joejaden1 <joejaden92@gmail.com>
2025-05-28 14:18:56 +0100

    upbot.py

--- bot.py
+++ bot.py
@@ -4,15 +4,15 @@ from modules_binance_trade import trade
 from config import TELEGRAM_TOKEN
 
 def main():
-   init_db()
-   updater = Updater(TELEGRAM_TOKEN, use_context=True)
-   dp = updater.dispatcher
+    init_db()
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
+    dp = updater.dispatcher
 
-   dp.add_handler(CommandHandler("trade", trade))
+    dp.add_handler(CommandHandler("trade", trade))
 # يمكنك إضافة المزيد من الأوامر هنا
 
-   updater.start_polling()
-   updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
-   main()
+    main()
