db6081b058ae8d4b1bb800a2fa9dbf8447dfc6ae
joejaden1 <joejaden92@gmail.com>
2025-05-29 02:18:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,3 @@ import os
 BINANCE_API_KEY = ("BINANCE_API_KEY")
 BINANCE_API_SECRET = ("BINANCE_API_SECRET")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-if not TELEGRAM_TOKEN:
-    raise RuntimeError("TELEGRAM_BOT_TOKEN is not set!")
-
-updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
