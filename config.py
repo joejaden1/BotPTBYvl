021e30363c7f57d5ed6bbf1b8c20b4d0b6609186
joejaden1 <joejaden92@gmail.com>
2025-05-29 02:04:51 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BINANCE_API_KEY = "ضع_مفتاح_باينانس_هنا"
-BINANCE_API_SECRET = "ضع_سر_باينانس_هنا"
-TELEGRAM_TOKEN = "ضع_توكن_بوت_تيليغرام_هنا"
+BINANCE_API_KEY = ("BINANCE_API_KEY")
+BINANCE_API_SECRET = ("BINANCE_API_SECRET")
+TELEGRAM_TOKEN = ("TELEGRAM_BOT_TOKEN")
